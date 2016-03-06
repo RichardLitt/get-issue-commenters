0b47838defa5c4e00ddc1b3a7e7262b64e5e4fa0
Richard Littauer <richard.littauer@gmail.com>
2016-03-06 14:07:31 -0500

    Update CLI and ghauth, depag
    Closes #3, closes #4

--- cli.js
+++ cli.js
@@ -1,22 +1,33 @@
 #!/usr/bin/env node
 'use strict'
 
-var meow = require('meow')
-var getIssueCommenters = require('./')
-var Promise = require('bluebird')
+const meow = require('meow')
+const getIssueCommenters = require('./')
+const Promise = require('bluebird')
+const gitconfig = require('gitconfiglocal')
+const pify = require('pify')
+const ghauth = Promise.promisify(require('ghauth'))
+const authOptions = {
+  configName: 'ghIssueCommenters',
+  note: 'Get GitHub issue commenters',
+  userAgent: 'ghIssueCommenters',
+  scope: ['repo']
+}
 
 const cli = meow(`
     Usage
       $ get-issue-commenters <input>
 
     Options
-      -s, --since Add a time since
-      -u, --until Add a time to
-      -r, --repo  Search in a specific repo
+    -r, --repo Only for a specific repo. [Default: false]
+    -s, --since Only since a specific time. [Default: false]
+    -u, --until Only to a specific time. [Default: false]
 
     Examples
+      $ get-issue-commenters
+      RichardLitt
       $ get-issue-commenters RichardLitt --since=2016-01-15T00:20:24Z --until=2016-01-20T00:20:24Z --repo=get-issue-commenters
-
+      RichardLitt
 `, {
   alias: {
     r: 'repo',
@@ -25,8 +36,20 @@ const cli = meow(`
   }
 })
 
-Promise.try(function () {
-  return getIssueCommenters(cli.input[0], cli.flags)
-}).map((response) => {
+Promise.try(() => {
+  return pify(gitconfig)(process.cwd())
+}).then((config) => {
+  if (config && config.remote && config.remote.origin && config.remote.origin.url) {
+    return config.remote.origin.url.split(':')[1].split('.git')[0].split('/')
+  }
+}).then((res) => {
+  if (res && cli.input.length === 0) {
+    cli.input[0] = res[0]
+    cli.flags['repo'] = res[1]
+  }
+  return ghauth(authOptions)
+}).then((authData) => {
+  return getIssueCommenters(cli.input[0], cli.flags, authData.token)
+}).map(function (response) {
   console.log(response)
 })
--- index.js
+++ index.js
@@ -1,78 +1,68 @@
 'use strict'
 
 const Octokat = require('octokat')
-const octo = new Octokat({
-  token: process.env.GITHUB_OGN_TOKEN
-})
+var octo
 const Promise = require('bluebird')
 const moment = require('moment')
 const _ = require('lodash')
 const depaginate = require('depaginate')
 const getGithubUser = require('get-github-user')
 
-module.exports = function (org, flags) {
-  function getAllRepos (org, flags) {
-    return Promise.resolve(getGithubUser(org))
-    .then((user) => {
-      if (user.length === 0) {
-        throw new Error(org + 'is not a valid GitHub user')
-      } else {
-        return user
-      }
-    })
-    .then(user => (user.type === 'Organization') ? octo.orgs(org).repos.fetch() : octo.users(org).repos.fetch())
-    .map((repo) => {
-      return depaginate((opts) => {
-        return octo.repos(opts.org, opts.repoName).issues.comments.fetch({
-          since: opts.since,
-          page: opts.page
-        })
-      }, {
-        org: org,
-        repoName: repo.name,
-        page: 1,
-        since: flags.since || '1970-01-01T00:01:03Z'
-      })
-    }).then(_.flatten.bind(_))
-  }
+module.exports = function (org, opts, token) {
+  octo = new Octokat({
+    token: token || process.env.GITHUB_OGN_TOKEN
+  })
 
-  function getRepo (org, flags) {
-    return Promise.resolve().then(() => {
-      return depaginate((opts) => {
-        return octo.repos(opts.org, opts.repoName).issues.comments.fetch({
-          since: opts.since,
-          page: opts.page
-        })
-      }, {
-        org: org,
-        repoName: flags.repo,
-        page: 1,
-        since: flags.since || '1970-01-01T00:01:03Z'
-      })
-    })
-  }
-
-  if (flags.since && !moment(flags.since).isValid()) {
+  if (opts.since && !moment(opts.since).isValid()) {
     throw new Error('Since flag is an invalid date.')
   }
-  if (flags.until && !moment(flags.until).isValid()) {
+  if (opts.until && !moment(opts.until).isValid()) {
     throw new Error('Until flag is an invalid date.')
   }
 
-  return Promise.resolve()
-  .then(() => (flags.repo) ? getRepo(org, flags) : getAllRepos(org, flags))
-  .filter(response => {
-    if (flags.since && flags.until && moment(response.updatedAt).isBetween(flags.since, flags.until)) {
+  return Promise.resolve(getGithubUser(org))
+  .then((user) => {
+    if (user.length === 0) {
+      throw new Error(org + 'is not a valid GitHub user')
+    } else {
+      return user
+    }
+  })
+  .map((user) => {
+    return depaginate(function (opts) {
+      return (opts.org.type === 'Organization') ? octo.orgs(org).repos.fetch(opts) : octo.users(org).repos.fetch(opts)
+    }, {
+      org: user
+    })
+  })
+  .then(_.flatten.bind(_))
+  .filter((response) => (opts.repo) ? response.name === opts.repo : response)
+  .map((repo) => {
+    return depaginate(function (opts) {
+      return octo.repos(opts.org, opts.repoName).issues.comments.fetch(opts)
+    }, {
+      org: org,
+      repoName: repo.name,
+      since: opts.since || '1980-01-01T00:01:01Z'
+    })
+  })
+  .then(_.flatten.bind(_))
+  .filter(function (response) {
+    if (opts.since && opts.until && moment(response.updatedAt).isBetween(opts.since, opts.until)) {
       return response
-    } else if (flags.since && !flags.until && moment(response.updatedAt).isAfter(flags.since)) {
+    } else if (opts.since && !opts.until && moment(response.updatedAt).isAfter(opts.since)) {
       return response
-    } else if (!flags.since && flags.until && moment(response.updatedAt).isBefore(flags.until)) {
+    } else if (!opts.since && opts.until && moment(response.updatedAt).isBefore(opts.until)) {
       return response
-    } else if (!flags.since && !flags.until) {
+    } else if (!opts.since && !opts.until) {
       return response
     }
   })
-  .map(response => response.user.login)
+  .map((response) => {
+    if (response.user && response.user.login) {
+      return response.user.login
+    }
+  })
   .then(response => _.uniq(_.without(response, undefined)))
   .catch(function (err) {
     console.log('err', err)
